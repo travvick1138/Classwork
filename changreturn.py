fbdafbc08eb192f9e1558278e72a36f7f6a99d64
Travis Vick <Travis Vick>
2016-05-19 16:36:50 -0700

    Aftrnoonwork5192016

--- changreturn.py
+++ changreturn.py
@@ -1,30 +1,28 @@
-#setup
+# setup
 Quarters = 25
 Dimes = 10
 Nickels = 5
 Cents = 1
 
-#input
+# input
 input_cents = int(input("How many cents do you have? "))
 
-
-#transformation
+# transformation
 number_of_quarters = input_cents // Quarters
-number_of_dimes = input_cents % Quarters // Dimes
-number_of_nickels = input_cents % Quarters // Dimes // Nickels
-number_of_cents = input_cents % Quarters // Dimes // Nickels // Cents
+number_of_dimes = (input_cents - (Quarters*number_of_quarters)) // Dimes
+number_of_nickels = (input_cents - ((Quarters*number_of_quarters)+(Dimes*number_of_dimes))) // Nickels
+number_of_cents = (input_cents - ((Quarters*number_of_quarters)+(Dimes*number_of_dimes)+(Nickels*number_of_nickels))) // Cents
 
-
-#output
+# output
 Your_change = '''                 Quarters = {Quarters}
                  Dimes    = {Dimes}
                  Nickels  = {Nickels}
-                 Cents    = {Cents}''' .format(
+                 Cents    = {Cents}'''.format(
 
-                 Cents=number_of_cents,
-                 Nickels=number_of_nickels,
-                 Dimes=number_of_dimes,
-                 Quarters=number_of_quarters
-                 )
+    Cents=number_of_cents,
+    Nickels=number_of_nickels,
+    Dimes=number_of_dimes,
+    Quarters=number_of_quarters
+)
 
 print(Your_change)
