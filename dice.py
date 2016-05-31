d480b450b7c905b3007a27887539ce84c37bd926
Travis Vick <Travis Vick>
2016-05-31 14:02:58 -0700

    testing

--- dice.py
+++ dice.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 def dice(number_of_dice, number_of_sides):
     for  die in range(1, number_of_dice + 1):
         print("Die number " + str(die) + ": " + str(randint(1, number_of_sides)))
@@ -8,3 +9,4 @@ dice_number = int(input("How many dice you like to roll?"))
 dice_sides = int(input("How many sides should each die have?"))
 
 dice(dice_number, dice_sides)
+
