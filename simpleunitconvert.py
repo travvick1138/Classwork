78ab4e5c70ae7ef5d10ec626044b50dd18ce46c0
Travis Vick <Travis Vick>
2016-06-03 11:06:28 -0700

    simpleunitconvert.py verified

--- simpleunitconvert.py
+++ simpleunitconvert.py
@@ -3,14 +3,17 @@ feet_in_a_mile = 5280
 
 #input
 
-input_mile = int(input("How many miles? "))
+input_mile = float(input("How many miles? "))
 
 #transformation
 
+miles = input_mile
 feet = (input_mile*feet_in_a_mile)
 
 
 #output
-print('{feet}' .format(
-        feet=feet
-))
+how_many_feet = '''This is how many feet {feet} in this many miles {miles}.''' .format(
+        feet=feet,
+        miles=miles
+)
+print(how_many_feet)
