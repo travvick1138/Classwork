02008f4cfb7ce935fa1ef400df94af7cc11180a1
Travis Vick <Travis Vick>
2016-06-03 10:36:16 -0700

    dice.py verified

--- Alexhasschangereturn.py
+++
@@ -1,8 +0,0 @@
-num_cents = int(input( "How much change? :"))
-
-quarters_in_change = num_cents // 25
-dime_in_change = num_cents % 25 // 10
-nickels_in_change = num_cents % 25 // 10 // 5
-pennies_in_change = num_cents % 25 // 10 // 1
-
-print("You have" + str(quarters_in_change) + "quarters," + str(dime_in_change) + "dimes," + str(nickels_in_change) + "nickels, and"  + str(pennies_in_change) + "pennies.")
